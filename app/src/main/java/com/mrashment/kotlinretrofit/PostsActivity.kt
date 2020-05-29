6879e256407ebbea264da7d55b9cf1147526e170
Mason Ashment <masonashment@gmail.com>
2020-05-28 21:47:06 -0400

    Initial commit

---
+++ app/src/main/java/com/mrashment/kotlinretrofit/PostsActivity.kt
@@ -0,0 +1,14 @@
+package com.mrashment.kotlinretrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posts)
+
+
+    }
+}
