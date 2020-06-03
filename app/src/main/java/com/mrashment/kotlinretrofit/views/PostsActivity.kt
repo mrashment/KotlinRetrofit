1ccd7fed70e74cc837ad17be4a94b9512b858dd0
Mason Ashment <masonashment@gmail.com>
2020-06-02 22:47:33 -0400

    getting users

--- app/src/main/java/com/mrashment/kotlinretrofit/views/PostsActivity.kt
+++ app/src/main/java/com/mrashment/kotlinretrofit/views/PostsActivity.kt
@@ -11,6 +11,7 @@ import com.mrashment.kotlinretrofit.R
 import com.mrashment.kotlinretrofit.adapters.PostAdapter
 import com.mrashment.kotlinretrofit.models.JsonPlaceholderApi
 import com.mrashment.kotlinretrofit.models.Post
+import com.mrashment.kotlinretrofit.models.User
 import com.mrashment.kotlinretrofit.repo.Repository
 import kotlinx.android.synthetic.main.activity_posts.*
 import retrofit2.Call
@@ -22,14 +23,34 @@ import retrofit2.converter.gson.GsonConverterFactory
 class PostsActivity : AppCompatActivity() {
 
     val posts: ArrayList<Post> = ArrayList()
+    val users: HashMap<Int,User> = HashMap()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
 
+        /* Find some way of making this ready for when posts are displayed
+        val userCall = Repository.getAllUser()
+        userCall.enqueue(object: Callback<List<User>> {
+            override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                Toast.makeText(this@PostsActivity,"Failed to fetch users",Toast.LENGTH_SHORT).show()
+            }
 
+            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                if (!response.isSuccessful) {
+                    Toast.makeText(this@PostsActivity,"Failed to fetch users",Toast.LENGTH_SHORT).show()
+                    return
+                }
+                for (u in response.body()!!.iterator()) {
+                    users.put(u.id,u)
+                }
+            }
+        })
+
+         */
 
         val call = Repository.getAllPosts()
+
         call.enqueue(object: Callback<List<Post>> {
             override fun onFailure(call: Call<List<Post>>, t: Throwable) {
                 Toast.makeText(this@PostsActivity,"Failed to retrieve data" + t.message,Toast.LENGTH_SHORT).show()
@@ -51,7 +72,6 @@ class PostsActivity : AppCompatActivity() {
 
         val recycler= recyclerView
         recycler.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
-        recycler.adapter = PostAdapter(posts)
 
 
     }
