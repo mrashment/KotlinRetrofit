3dbe7225555ac99b13b622479ba78f8bd8cb8fe8
Mason Ashment <masonashment@gmail.com>
2020-06-05 22:56:08 -0400

    album should show user id too

--- app/src/main/java/com/mrashment/kotlinretrofit/views/AlbumsActivity.kt
+++ app/src/main/java/com/mrashment/kotlinretrofit/views/AlbumsActivity.kt
@@ -2,6 +2,9 @@ package com.mrashment.kotlinretrofit.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.mrashment.kotlinretrofit.R
 
 class AlbumsActivity : AppCompatActivity() {
@@ -9,5 +12,20 @@ class AlbumsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_albums)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_posts,menu)
+        return true
+    }
+
+    fun showAll() {}
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.optionAll -> showAll()
+            else -> Toast.makeText(this@AlbumsActivity,"Options menu did something whacky", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
