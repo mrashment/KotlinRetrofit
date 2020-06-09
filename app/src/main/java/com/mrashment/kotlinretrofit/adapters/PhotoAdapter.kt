cf3e926724c7c7e0c0104856dea9e9f2cb8bd43d
Mason Ashment <masonashment@gmail.com>
2020-06-09 17:18:23 -0400

    Thumbnails still not working. Added a placeholder.

--- app/src/main/java/com/mrashment/kotlinretrofit/adapters/PhotoAdapter.kt
+++ app/src/main/java/com/mrashment/kotlinretrofit/adapters/PhotoAdapter.kt
@@ -1,5 +1,7 @@
 package com.mrashment.kotlinretrofit.adapters
 
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +10,7 @@ import com.bumptech.glide.Glide
 import com.mrashment.kotlinretrofit.R
 import com.mrashment.kotlinretrofit.models.Photo
 import kotlinx.android.synthetic.main.layout_photo_cardview.view.*
+import java.net.URL
 
 class PhotoAdapter(val photos: ArrayList<Photo>, val photoClickListener: (Photo) -> Unit): RecyclerView.Adapter<PhotoAdapter.PhotoHolder>() {
 
@@ -24,11 +27,16 @@ class PhotoAdapter(val photos: ArrayList<Photo>, val photoClickListener: (Photo)
     }
 
     class PhotoHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val TAG = "PhotoHolder"
 
         fun bind(photo: Photo, photoClickListener: (Photo) -> Unit) {
             itemView.tvId.text = photo.id.toString()
             itemView.tvTitle.text = photo.title
-            Glide.with(itemView.context).load(photo.thumbnailUrl).into(itemView.ivThumbnail)
+            Log.d(TAG,photo.thumbnailUrl)
+            Glide.with(itemView)
+                .load(Uri.parse(photo.thumbnailUrl))
+                .placeholder(R.mipmap.ic_launcher)
+                .into(itemView.ivThumbnail)
             itemView.setOnClickListener { photoClickListener(photo)}
         }
     }
